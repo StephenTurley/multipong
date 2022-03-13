2bec578c628d26c1518fe1c46a1d2149ecefc62c
Stephen Turley <stephen@turley.dev>
2022-03-13 11:52:01 -0400

    added prettier

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,4 +1,4 @@
-import * as Phaser from "phaser"
+import * as Phaser from "phaser";
 
 const start = () => {
   const config = {
@@ -6,40 +6,38 @@ const start = () => {
     width: 800,
     height: 600,
     physics: {
-      default: 'arcade',
+      default: "arcade",
       arcade: {
-        gravity: { y: 200 }
-      }
+        gravity: { y: 200 },
+      },
     },
     scene: {
       preload: preload,
-      create: create
-    }
+      create: create,
+    },
   };
   var game = new Phaser.Game(config);
 
-  function preload ()
-  {
-    this.load.setBaseURL('https://labs.phaser.io');
+  function preload() {
+    this.load.setBaseURL("https://labs.phaser.io");
 
-    this.load.image('sky', 'assets/skies/space3.png');
-    this.load.image('logo', 'assets/sprites/phaser3-logo.png');
-    this.load.image('red', 'assets/particles/red.png');
+    this.load.image("sky", "assets/skies/space3.png");
+    this.load.image("logo", "assets/sprites/phaser3-logo.png");
+    this.load.image("red", "assets/particles/red.png");
   }
 
-  function create ()
-  {
-    this.add.image(400, 300, 'sky');
+  function create() {
+    this.add.image(400, 300, "sky");
 
-    var particles = this.add.particles('red');
+    var particles = this.add.particles("red");
 
     var emitter = particles.createEmitter({
       speed: 100,
       scale: { start: 1, end: 0 },
-      blendMode: 'ADD'
+      blendMode: "ADD",
     });
 
-    var logo = this.physics.add.image(400, 100, 'logo');
+    var logo = this.physics.add.image(400, 100, "logo");
 
     logo.setVelocity(100, 200);
     logo.setBounce(1, 1);
